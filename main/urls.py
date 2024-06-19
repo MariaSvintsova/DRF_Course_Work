cc8b1c382392c71407f255f414ab480d34e48f8d
MariaSvintsova <m.svintsova@it-park.tech>
2024-06-19 13:35:53 +0300

    error 2

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from main.views import HabitDestroyAPIView, HabitUpdateAPIView, HabitCreateView, HabitViewSet, HabitDetailView
 
 urlpatterns = [
-    path('habits/', HabitViewSet.as_view({'get': 'list'}), name='habit-list'),
+    path('/', HabitViewSet.as_view(), name='habit-list'),
     path('<int:pk>/', HabitDetailView.as_view(), name='habit-detail'),
     path('create/', HabitCreateView.as_view(), name='habit-create'),
     path('/update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit-update'),
